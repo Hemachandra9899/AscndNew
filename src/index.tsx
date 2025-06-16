b286eed15eda2b8b3b12a5580c61467585c0019c
Hemachandra9899 <130845013+Hemachandra9899@users.noreply.github.com>
2025-06-16 19:49:00 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MacbookAir } from "./screens/MacbookAir";
+import {MensCollectionPage} from "./screens/MacbookAir/MensCollectionPage";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MacbookAir } from "./screens/MacbookAir";
+import  { ProductDetailPage } from "./screens/MacbookAir/ProductDetailPage";
+
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <MacbookAir />
+  
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MacbookAir />} />
+      <Route path="/mens" element={<MensCollectionPage />} />
+      <Route path="/product" element={<ProductDetailPage />} />
+    </Routes>
+  </BrowserRouter>
   </StrictMode>,
 );
