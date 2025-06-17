eabc60a8cd68b4160e9cc71b5ffdeed2a2aa73d5
Hemachandra9899 <130845013+Hemachandra9899@users.noreply.github.com>
2025-06-17 22:58:34 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import {MensCollectionPage} from "./screens/MacbookAir/MensCollectionPage";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import  { ProductDetailPage } from "./screens/MacbookAir/ProductDetailPage";
+import { OurFabricTechnology } from "./screens/MacbookAir/OurFabricTechnology";
 
 
 createRoot(document.getElementById("app") as HTMLElement).render(
@@ -14,6 +15,7 @@ createRoot(document.getElementById("app") as HTMLElement).render(
       <Route path="/" element={<MacbookAir />} />
       <Route path="/mens" element={<MensCollectionPage />} />
       <Route path="/product" element={<ProductDetailPage />} />
+      <Route path="/fabric" element={<OurFabricTechnology />} />//must use lowercase letters 
     </Routes>
   </BrowserRouter>
   </StrictMode>,
